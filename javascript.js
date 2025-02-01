0670b1f5a6858f708d633736150f581063fd8668
kamimarinn <kacavalcante@hotmail.com>
2025-02-01 16:53:51 -0300

    add: just examples

--- javascript.js
+++ javascript.js
@@ -9,8 +9,22 @@ const c = 10
 //n podemos reassinar uma variavel constante
 //c = 11
 
+a = 'banana'
+b = 500
 
 //comunicação via terminal
-
+//descobrir um tipo
+console.log(a, b, c)
 console.log ('rodei o script!')
 
+//tipos de dados
+let name = 'Lula'
+let age = 120
+let open = false
+const person = {
+    name:'felipe',
+    age: 15,
+    gender: 'male'
+}
+
+const persons = ['felipe', 'maria', 'joao']
