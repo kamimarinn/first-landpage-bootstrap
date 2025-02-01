17f243ef24ebc06badebb091a6280d4372d0a972
kamimarinn <kacavalcante@hotmail.com>
2025-02-01 14:34:09 -0300

    feat:addnode

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// variables
+
+var a //que varia sempre
+
+let b
+
+const c = 10
+
+//n podemos reassinar uma variavel constante
+//c = 11
+
+
+//comunicação via terminal
+
+console.log ('rodei o script!')
+
